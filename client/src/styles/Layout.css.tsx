e790e4e9b1f941373601820142278b96c6d3f9cb
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-02 17:59:27 +0530

    style: made layout responsive

--- client/src/styles/Layout.css.tsx
+++ client/src/styles/Layout.css.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import { media } from "./Responsive.css";
 
 export const Container = styled.div`
-    background-color: var(--primary-bg);
+	background-color: var(--primary-bg);
 	height: 100vh;
 	position: relative;
 `;
@@ -9,7 +10,10 @@ export const Container = styled.div`
 export const Main = styled.main`
 	height: 100%;
 	position: relative;
-    /* &:nth-child(2) {
-        padding: 4em 0 0 9.5em;
-    } */
+
+	${media.lg`    
+        &:nth-child(2) {
+            padding: 4em 0 0 9em;
+        }
+    `}
 `;
