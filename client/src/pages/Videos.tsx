478e10009ede5525a5f7f240be708a5c9c1bebf1
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-08 16:27:27 +0530

    feat: Implemented filtering

--- client/src/pages/Videos.tsx
+++ client/src/pages/Videos.tsx
@@ -1,8 +1,9 @@
-import { LoaderFunctionArgs, useLoaderData } from "react-router-dom";
-import { Container, CatCont, FilterTitle } from "../styles/Videos.css";
+import { LoaderFunctionArgs, useLoaderData, useSearchParams } from "react-router-dom";
+import { Container, CatCont, Wrapper } from "../styles/Videos.css";
 import { getAllVideos } from "../utils/api";
 import VideoCard from "../components/VideoCard";
 import IVideo from "../types/video";
+import { Link } from "react-router-dom";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
 	const data = await getAllVideos({ request } as LoaderFunctionArgs);
@@ -11,15 +12,45 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
 const Videos = () => {
 	const videos = useLoaderData() as IVideo[];
+	const [searchParams] = useSearchParams();
+	const cat = searchParams.get("cat");
 
 	return (
 		<Container>
-			{videos?.map((video, idx) => (
-				<VideoCard
-					key={idx}
-					video={video}
-				/>
-			))}
+			<CatCont>
+				<Link
+					to="/host/videos"
+					className={cat === null ? "active" : ""}
+				>
+					All
+				</Link>
+				<Link
+					to="/host/videos?cat=JavaScript"
+					className={cat === "JavaScript" ? "active" : ""}
+				>
+					JavaScript
+				</Link>
+				<Link
+					to="/host/videos?cat=TypeScript"
+					className={cat === "TypeScript" ? "active" : ""}
+				>
+					TypeScript
+				</Link>
+				<Link
+					to="/host/videos?cat=React"
+					className={cat === "React" ? "active" : ""}
+				>
+					React
+				</Link>
+			</CatCont>
+			<Wrapper>
+				{videos?.map((video, idx) => (
+					<VideoCard
+						key={idx}
+						video={video}
+					/>
+				))}
+			</Wrapper>
 		</Container>
 	);
 };
--- client/src/styles/Videos.css.tsx
+++ client/src/styles/Videos.css.tsx
@@ -2,30 +2,38 @@ import styled from "styled-components";
 import { media } from "./Responsive.css";
 
 export const Container = styled.div`
-    background-color: var(--primary-bg);
-	display: flex;
-	justify-content: center;
-	flex-wrap: wrap;
-	gap: 1em;
+	background-color: var(--primary-bg);
 	padding: 0.2em 0.2em 6em 0.2em;
-	& > * {
-		flex: 1 1 300px;
-	}
 
 	${media.lg`
         padding-bottom: 1em;
     `}
 `;
 
-export const CatCont = styled.div`
+export const Wrapper = styled.div`
 	display: flex;
-	margin-top: 1em;
+	justify-content: center;
+	flex-wrap: wrap;
+	gap: 1em;
 	& > * {
-		background-color: var(--white-cl);
-		margin-left: 1.5em;
-		padding: 1em 2em;
-		cursor: pointer;
+		flex: 1 1 300px;
 	}
 `;
 
-export const FilterTitle = styled.span``;
+export const CatCont = styled.div`
+	padding: 1em 0;
+	& > a {
+		display: inline-block;
+		background-color: var(--white-cl);
+		color: #333;
+		padding: 0.3125em 2em;
+		margin: 0.3125em;
+		border-radius: 0.3125em;
+        box-shadow: -0.1em 0.1em 0.3125em 0.1em #3333335c;
+	}
+	& > a.active,
+	& > a:hover {
+		background-color: #333;
+		color: var(--primary-bg);
+	}
+`;
