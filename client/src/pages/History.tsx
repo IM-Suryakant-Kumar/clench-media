cd0d4651c58d8da37eade00c8fbe7eaae8feb418
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-09 17:34:06 +0530

    feat: added History page functionality

--- client/src/pages/History.tsx
+++ client/src/pages/History.tsx
@@ -1,5 +1,9 @@
 import { useLoaderData } from "react-router-dom";
-import { getAllHistoryVideos } from "../utils/api";
+import { DeleteFromHistory, getAllHistoryVideos } from "../utils/api";
+import { Container, DeleteBtn, Wrapper } from "../styles/Like.css";
+import IVideo from "../types/video";
+import { useState } from "react";
+import VideoCard from "../components/VideoCard";
 
 export const loader = async () => {
 	const data = await getAllHistoryVideos();
@@ -7,10 +11,27 @@ export const loader = async () => {
 };
 
 const History = () => {
-	const historyVideos = useLoaderData();
-	console.log(historyVideos);
+	const savedVideos = useLoaderData() as IVideo[];
+	const [videos, setVideos] = useState<IVideo[]>(savedVideos);
 
-	return <div>History</div>;
+	const handleClick = async (videoId: string) => {
+		const data = await DeleteFromHistory(videoId);
+		data.success && setVideos((prevVideos) => prevVideos.filter((v) => v.videoId !== videoId));
+	};
+
+	return (
+		<Container>
+			{videos.map((video, idx) => (
+				<Wrapper key={idx}>
+					<VideoCard video={video} />
+					<DeleteBtn
+						size="2rem"
+						onClick={() => handleClick(video.videoId)}
+					/>
+				</Wrapper>
+			))}
+		</Container>
+	);
 };
 
 export default History;
