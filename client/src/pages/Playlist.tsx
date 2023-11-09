47d2ff4f4f83b16ab0bb955e98d84e1a4d2a4c2e
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-09 19:53:13 +0530

    feat: delete playlists functionality

--- client/src/pages/Playlist.tsx
+++ client/src/pages/Playlist.tsx
@@ -2,13 +2,13 @@ import { useLoaderData } from "react-router-dom";
 import { getAllPlaylist } from "../utils/api";
 import IVideo from "../types/video";
 import { useState } from "react";
-import { Container } from "../styles/Playlist.css.tsx";
+import { Container, DeleteBtn, Wrapper } from "../styles/Playlist.css.tsx";
 import PlaylistCard from "../components/PlaylistCard.tsx";
 
 interface Playlist {
 	name: string;
 	video: IVideo;
-    numOfVideos: number
+	numOfVideos: number;
 }
 
 export const loader = async () => {
@@ -20,17 +20,28 @@ const Playlist = () => {
 	const allPlaylist = useLoaderData() as Playlist[];
 	const [playlists, setPlaylists] = useState<Playlist[]>(allPlaylist);
 
-	console.log(playlists);
+	const handleClick = async (name: string) => {
+		console.log(name);
+		// const data = await deleteLike(videoId)
+		// data.success && setVideos(prevVideos => prevVideos.filter(v => v.videoId !== videoId))
+		setPlaylists((prevPlaylists) => prevPlaylists.filter((item) => item.name !== name));
+	};
 
 	return (
 		<Container>
 			{playlists.map((item, idx) => (
-				<PlaylistCard
-					key={idx}
-					name={item.name}
-					video={item.video}
-                    numOfVideos={item.numOfVideos}
-				/>
+				<Wrapper key={idx}>
+					<PlaylistCard
+						name={item.name}
+						video={item.video}
+						numOfVideos={item.numOfVideos}
+					/>
+					<DeleteBtn
+						size="2rem"
+						color="#ff607d"
+						onClick={() => handleClick(item.name)}
+					/>
+				</Wrapper>
 			))}
 		</Container>
 	);
