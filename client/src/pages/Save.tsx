2db2a24abc7e0f38db0acc7831de073b569d4f97
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-09 17:33:00 +0530

    feat: added Save page functionality

--- client/src/pages/Save.tsx
+++ client/src/pages/Save.tsx
@@ -1,5 +1,9 @@
 import { useLoaderData } from "react-router-dom"
-import { getAllSavedVideos } from "../utils/api"
+import { deleteFromSave, getAllSavedVideos } from "../utils/api"
+import { Container, DeleteBtn, Wrapper } from "../styles/Like.css";
+import IVideo from "../types/video";
+import { useState } from "react";
+import VideoCard from "../components/VideoCard";
 
 export const loader = async () => {
     const data = await getAllSavedVideos()
@@ -7,11 +11,25 @@ export const loader = async () => {
 }
 
 const Save = () => {
-    const savedVideos = useLoaderData()
-    console.log(savedVideos)
+    const savedVideos = useLoaderData() as IVideo[]
+    const [videos , setVideos] = useState<IVideo[]>(savedVideos)
+
+    const handleClick = async (videoId: string) => {
+        const data = await deleteFromSave(videoId)
+        data.success && setVideos(prevVideos => prevVideos.filter(v => v.videoId !== videoId))
+    }
 
   return (
-    <div>Save</div>
+    <Container>
+			{videos.map((video, idx) => (
+				<Wrapper key={idx}>
+					<VideoCard
+						video={video}
+					/>
+                    <DeleteBtn size="2rem" onClick={() => handleClick(video.videoId)} />
+				</Wrapper>
+			))}
+		</Container>
   )
 }
 
